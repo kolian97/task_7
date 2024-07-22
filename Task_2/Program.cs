18ee5c19c59166cc4615aa18df1e4d60f135e974
kolian97 <kolian_97@mail.ru>
2024-07-22 09:32:34 +0300

    Добавил остальное

---
+++ Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n. 
+
+// m = 2, n = 3 -> A(m,n) = 29
+
+//A(m,n) а не A(n,m)!!
+void Main()
+{
+    int m = Read_int("Введите Значение m: ");
+    int n = Read_int("Введите Значение n: ");
+    System.Console.WriteLine(A(m,n));
+}
+int Read_int(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int A(int m, int n)
+{
+    if(m ==0)
+    return n + 1;
+    else if(n==0)
+    return A(m-1,1);
+    else
+    return A(m - 1,A(m,n-1));
+}
+Main();
---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+// Пример
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+void Main()
+{
+    int[] arras = [1, 2, 5, 0, 10, 34];
+    on_the_contrary(arras);
+}
+void on_the_contrary(int[] array,int count = 0)
+{
+    if(count > array.Length - 1) return;
+    on_the_contrary(array,count + 1);
+    System.Console.Write(array[count] + " ");
+}
+
+Main();
